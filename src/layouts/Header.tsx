8afb0a59e39de13a92f20b7e81e32a0894a52ee2
burkush <avburkush@gmail.com>
2023-10-22 21:36:16 +0300

    initial commit

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import logo from '/vite.svg';
 import { Link } from 'react-router-dom';
 
--- src/pages/SearchResult.tsx
+++ src/pages/SearchResult.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import SearchBar from '../components/SearchBar';
 import Movie from '../components/Movies/Movie';
 import { IMovie } from '../models/IMovie';
